c48b1966658cddc5277d728bb48d6233fb3a30f5
Amolin10 <molina102997@gmail.com>
2022-08-04 10:28:27 -0500

    10:28 am Examen 1

---
+++ Examenes/Examen1/src/com/Interfaces/Cuadrado.java
@@ -0,0 +1,21 @@
+package com.Interfaces;
+
+public class Cuadrado implements FiguraGeometrica {
+
+	private float lado;
+	
+	public Cuadrado(float lado) {
+		this.lado = lado;
+	}
+	
+	@Override
+	public float getArea() {
+		return lado * lado;
+	}
+
+	@Override
+	public String toString() {
+		return "Área: " + this.getClass().getSimpleName() + ": L*L";
+	}
+
+}
